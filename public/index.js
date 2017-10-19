5a810282fcbfe1d026d874a5c2c7c26a73e76c81
Tim Reilly <timreilly@live.com>
2017-10-19 13:34:25 -0700

    configured for full zoom and moved script to index.js

--- public/index.js
+++ public/index.js
@@ -1,1 +1,21 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var map;
+var ajaxRequest;
+var plotlist;
+var plotlayers = [];
+
+function initmap() {
+    // set up the map
+    map = new L.Map('mapid');
+
+    // create the tile layer with correct attribution
+    var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, { minZoom: 8, maxZoom: 20, attribution: osmAttrib });
+
+    // start the map in South-East England
+    map.setView(new L.LatLng(37.78, -122.44), 14);
+    map.addLayer(osm);
+}
+
+initmap();
+

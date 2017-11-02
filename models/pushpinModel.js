f280702976faed9be8015795953a96d9b0634e7c
Maribel Duran <maribelduranucm@gmail.com>
2017-11-01 20:09:09 -0700

    Update pushpin model.

--- models/pushpinModel.js
+++ models/pushpinModel.js
@@ -2,19 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var pushpinModel = new Schema({
-    loc: {
-        bounds: [{
-            type: Number
-        }],
-        x: { type: String },
-        y: { type: String }
-    },
-    metadata: {
-        asset: { type: String },
-        author: { type: String },
-        description: { type: String },
-        title: { type: String }
-    }
+    //coordinates should be in [longitude, latitude]
+    geo: {
+      type: {type: String},
+      coordinates: { type: [Number] }
+   }, 
+    
+    asset: { type: String },
+    author: { type: String },
+    description: { type: String },
+    title: { type: String }
+
 });
 
 module.exports = mongoose.model('Pushpin', pushpinModel);
